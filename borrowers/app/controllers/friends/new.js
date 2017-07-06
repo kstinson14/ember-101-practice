b275803db125dcde608c4887105bc1d2191945dc
Katrina Stinson <katrina.m.stinson@gmail.com>
2017-07-06 14:08:04 -0400

    Added support for users to click/get details on individuals in the friend index.

--- borrowers/app/controllers/friends/new.js
+++ borrowers/app/controllers/friends/new.js
@@ -5,10 +5,11 @@ export default Ember.Controller.extend({
     save(model) {
       console.log('+---- save action called in friends/new controller');
 
+      this.transitionToRoute('friends.show', model);
     },
     cancel() {
       console.log('+---- cancel action called in friends/new controller');
-      
+
     }
   }
 });
